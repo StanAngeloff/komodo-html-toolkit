9479d788af98694f6b459bf25d2c4b97af064a7b
Stan Angeloff <insaned@abv.bg>
2009-05-09 14:41:32 +0000

    Updated regular expressions library with DOCTYPE, tag and attribute name sniffing. Updated TODO file to reflect recent changes.

--- src/content/scripts/regexp.js
+++ src/content/scripts/regexp.js
@@ -2,15 +2,18 @@ $self.patterns = {
 
 	'Tag': '<([a-zA-Z0-9\\-\\_\\:\\/]+)\\b',
 	'TagWord': '[<a-zA-Z0-9\\-\\_\\:]+>?',
+	'TagName': '[<\\/]*([a-zA-Z0-9\\-\\_\\:]+)\\b',
 	'OpeningTag': '<([a-zA-Z0-9\\-\\_\\:]+)',
 	'ClosedTag': '(<\\/([a-zA-Z0-9\\-\\_\\:]+)>)|(\\s*\\/>)',
 	'SnippetTag': '(<[\\/]?)html\\:([a-zA-Z0-9\\-\\_\\:]+)(\\b)',
+	'AttributeName': '[a-zA-Z0-9\\-\\_\\:]+\\b',
 	'Operator': '[\\/\\-\\?\\#\\%]+\\s*',
 	'OutOfTag': '(.<)|(<\\/)',
 	'Word': '\\w+',
 	'Whitespace': '\\s+',
 	'GeckoImageFormats': '(\.(?:png|apng|jpg|jpeg|gif|bmp|xbm|svg))',
-	'Protocol': '\\w+:\/\/'
+	'Protocol': '\\w+:\/\/',
+	'DoctypeDeclaration': '<!DOCTYPE\\s+(\\S+)\\s+(PUBLIC|SYSTEM)\\s+("[^"]*"|\'[^\']*\')\\s*("[^"]*"|\'[^\']*\')?\\s*(\\[|>)'
 };
 
 $self.match = function(regexp, context, prepend, append, modifiers) {
