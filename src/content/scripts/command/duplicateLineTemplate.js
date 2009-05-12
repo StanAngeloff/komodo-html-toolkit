f01f4ea64b80d48d2aacb4c037df3cd020f736fe
Stan Angeloff <insaned@abv.bg>
2009-05-12 15:56:20 +0000

    Issue 19 SUPPORTS: Duplicate Line as Template

---
+++ src/content/scripts/command/duplicateLineTemplate.js
@@ -0,0 +1,55 @@
+$toolkit.include('command');
+
+$self.controller = function() {
+
+	var isMac = (navigator.platform.indexOf('Mac') >= 0);
+
+	// Call parent's constructor
+	$toolkit.command.controller.apply(this, ['duplicateLineTemplate', [(isMac ? 'Meta' : 'Ctrl') + '+Alt+D'], true]);
+
+	this.trigger = function(e) {
+
+		var view = ko.views.manager.currentView,
+			scimoz = view.scimoz;
+
+		// Duplicate current line if no selection
+		if (scimoz.anchor !== scimoz.currentPos)
+			return false;
+
+		// If autocomplete is visible, close it
+		if (scimoz.autoCActive())
+			scimoz.autoCCancel();
+
+		// Copy entire line
+		var lineStartPosition = scimoz.positionFromLine(scimoz.lineFromPosition(scimoz.currentPos)),
+			lineEndPosition = scimoz.getLineEndPosition(scimoz.lineFromPosition(scimoz.currentPos)),
+			lineRange = scimoz.getTextRange(lineStartPosition, lineEndPosition);
+
+		var snippetValue = lineRange,
+			tabstopsLength = 0;
+
+		// Support for server-side languages; remove any escape characters and
+		// place tabstops inside quotes
+		snippetValue = snippetValue.replace(/\\"|\\'/g, '')
+								   .replace(/(["']{1}).*?\1/g, function(match, quoteStyle) { return quoteStyle + '[[%tabstop' + (++ tabstopsLength) + ']]' + quoteStyle; })
+
+		// Wrap final snippet
+		snippetValue = '\n' + snippetValue + '[[%tabstop0]]';
+
+		// Move cursor at line end; this is where we insert the wrapped snippet
+		scimoz.anchor = scimoz.currentPos = lineEndPosition;
+
+		Snippet_insert($toolkit.library.createSnippet(snippetValue, null, null, 'false', 'false'));
+
+		scimoz.scrollCaret();
+
+		return true;
+	};
+
+	$toolkit.trapExceptions(this);
+};
+
+$self.registerAll = function() {
+
+	new $self.controller().register();
+};
