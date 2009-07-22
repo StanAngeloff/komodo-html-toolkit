df6bd3451831a5754ebd33785aadf91fb21e2a16
Stan Angeloff <insaned@abv.bg>
2009-07-22 15:07:49 +0000

    Added checks in nonBreakingSpace.js to prevent insertion of entities while the caret is within a tag name or an attribute value.

--- src/content/scripts/command/nonBreakingSpace.js
+++ src/content/scripts/command/nonBreakingSpace.js
@@ -11,6 +11,23 @@ $self.controller = function() {
 
 		if ( ! this.stopUndo) {
 
+			// Make sure we are not inside a tag or an attribute
+			if ([scimoz.SCE_UDL_M_STAGO,
+				 scimoz.SCE_UDL_M_TAGNAME,
+				 scimoz.SCE_UDL_M_TAGSPACE,
+				 scimoz.SCE_UDL_M_ATTRNAME,
+				 scimoz.SCE_UDL_M_OPERATOR,
+				 scimoz.SCE_UDL_M_STRING,
+				 scimoz.SCE_UDL_M_STAGC,
+				 scimoz.SCE_UDL_M_ETAGO,
+				 scimoz.SCE_UDL_M_ETAGC,
+				 scimoz.SCE_UDL_M_EMP_TAGC].indexOf(scimoz.getStyleAt(scimoz.currentPos)) >= 0) {
+
+				ko.statusBar.AddMessage($toolkit.l10n('command').GetStringFromName('nonBreakingSpace.invalidStyle'), 'htmltoolkit', 1500, false);
+
+				return false;
+			}
+
 			this.stopUndo = true;
 
 			scimoz.beginUndoAction();
@@ -34,6 +51,8 @@ $self.controller = function() {
 		// Do not process event any further
 		e.preventDefault();
 		e.stopPropagation();
+
+		return true;
 	};
 
 	this.stop = function(e) {
