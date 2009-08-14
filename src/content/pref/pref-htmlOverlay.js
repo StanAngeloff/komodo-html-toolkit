5cad874d9167e1bc0c23e8234fd080daaf144a98
Stan Angeloff <insaned@abv.bg>
2009-08-14 22:17:36 +0000

    Started working on a Add-on Options dialogue. Need to sketch available options on paper before implementing the UI - very blurry.

--- src/content/pref/pref-htmlOverlay.js
+++ src/content/pref/pref-htmlOverlay.js
@@ -6,5 +6,17 @@ OnPreferencePageInitalize = function(prefset) {
 	// Call previous event handler
 	OnPreferencePageInitalizeBase(prefset);
 
-	// TODO: Collapse first spacer in document
+	// We need to remove Komodo's spacer at document end
+	var spacerEl = document.getElementsByTagName('spacer')[0];
+
+	if (spacerEl && spacerEl.parentNode &&
+		'window' === spacerEl.parentNode.tagName)
+		spacerEl.parentNode.removeChild(spacerEl);
+
+	// Bring up Options dialogue on button click
+	document.getElementById('htmltoolkitOptions').addEventListener('click', function() {
+
+		window.openDialog('chrome://htmltoolkit/content/pref/htmltoolkit.xul');
+
+	}, false);
 };
