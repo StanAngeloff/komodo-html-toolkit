bd07c04be1445e73574302fe234cd0f0014c80d0
Stan Angeloff <insaned@abv.bg>
2010-06-21 16:39:00 +0300

    Compatibility with Komodo 6 toolbox -- not as flexible as the old version, no option to have ":" in the snippet name and no support for folders.

--- src/content/scripts/command/abbreviation/toolbox.js
+++ src/content/scripts/command/abbreviation/toolbox.js
@@ -37,66 +37,73 @@ $self.provider = function() {
 		if (viewLanguages.indexOf('General') < 0)
 			viewLanguages.push('General');
 
-		var partService = Cc['@activestate.com/koPartService;1'].getService(Ci.koIPartService),
-			abbreviationsFolders = partService.getParts('folder', 'name', 'Abbreviations', '*', partService.currentProject, {}),
-			abbreviationName = abbreviation.split('.').shift(),
-			languageContainers, languageSnippetsContainer, languageSnippets;
+        if (typeof (ko.toolbox2) === 'undefined') {
 
-		for (var i = 0; i < abbreviationsFolders.length; i ++)
-			for (var j = 0; j < viewLanguages.length; j ++) {
+            var partService = Cc['@activestate.com/koPartService;1'].getService(Ci.koIPartService),
+                abbreviationsFolders = partService.getParts('folder', 'name', 'Abbreviations', '*', partService.currentProject, {}),
+                abbreviationName = abbreviation.split('.').shift(),
+                languageContainers, languageSnippetsContainer, languageSnippets;
 
-				// Find all sub-folders at top level
-				languageContainers = {};
-				abbreviationsFolders[i].getChildrenByType('folder', false, languageContainers, {});
-				if (languageContainers.value) {
+            for (var i = 0; i < abbreviationsFolders.length; i ++)
+                for (var j = 0; j < viewLanguages.length; j ++) {
 
-					languageContainers = languageContainers.value;
+                    // Find all sub-folders at top level
+                    languageContainers = {};
+                    abbreviationsFolders[i].getChildrenByType('folder', false, languageContainers, {});
+                    if (languageContainers.value) {
 
-					// Match the view language against the name of each folder
-					languageSnippetsContainer = null;
-					for (var k = 0; k < languageContainers.length; k ++)
-						if (viewLanguages[j].toLowerCase() === languageContainers[k].name.toLowerCase()) {
+                        languageContainers = languageContainers.value;
 
-							languageSnippetsContainer = languageContainers[k];
-							break;
-						}
+                        // Match the view language against the name of each folder
+                        languageSnippetsContainer = null;
+                        for (var k = 0; k < languageContainers.length; k ++)
+                            if (viewLanguages[j].toLowerCase() === languageContainers[k].name.toLowerCase()) {
 
-					// If we have found a folder matching our view language, look for a snippet inside
-					if (languageSnippetsContainer) {
+                                languageSnippetsContainer = languageContainers[k];
+                                break;
+                            }
 
-						languageSnippets = {};
-						// Find all snippets (incl. nested folders) inside the view language folder
-						languageSnippetsContainer.getChildrenByType('snippet', true, languageSnippets, {});
-						if (languageSnippets.value) {
+                        // If we have found a folder matching our view language, look for a snippet inside
+                        if (languageSnippetsContainer) {
 
-							languageSnippets = languageSnippets.value;
-							for (k = 0; k < languageSnippets.length; k ++) {
+                            languageSnippets = {};
+                            // Find all snippets (incl. nested folders) inside the view language folder
+                            languageSnippetsContainer.getChildrenByType('snippet', true, languageSnippets, {});
+                            if (languageSnippets.value) {
 
-								// Attempt a full-name match
-								if (languageSnippets[k].name == abbreviationName)
-									return languageSnippets[k];
-								// Attempt to match first word-part of the snippet name
-								else if (languageSnippets[k].name.split(/[\W\.]+/).shift() == abbreviationName)
-									return languageSnippets[k];
-							}
-						}
+                                languageSnippets = languageSnippets.value;
+                                for (k = 0; k < languageSnippets.length; k ++) {
 
-						languageSnippets = {};
-						// Find all folders (incl. nested folders) inside the view language folder
-						languageSnippetsContainer.getChildrenByType('folder', true, languageSnippets, {});
-						if (languageSnippets.value) {
+                                    // Attempt a full-name match
+                                    if (languageSnippets[k].name == abbreviationName)
+                                        return languageSnippets[k];
+                                    // Attempt to match first word-part of the snippet name
+                                    else if (languageSnippets[k].name.split(/[\W\.]+/).shift() == abbreviationName)
+                                        return languageSnippets[k];
+                                }
+                            }
 
-							languageSnippets = languageSnippets.value;
-							for (k = 0; k < languageSnippets.length; k ++) {
+                            languageSnippets = {};
+                            // Find all folders (incl. nested folders) inside the view language folder
+                            languageSnippetsContainer.getChildrenByType('folder', true, languageSnippets, {});
+                            if (languageSnippets.value) {
 
-								// Attempt a full-name match
-								if (languageSnippets[k].name == abbreviationName)
-									return languageSnippets[k];
-							}
-						}
-					}
-				}
-			}
+                                languageSnippets = languageSnippets.value;
+                                for (k = 0; k < languageSnippets.length; k ++) {
+
+                                    // Attempt a full-name match
+                                    if (languageSnippets[k].name == abbreviationName)
+                                        return languageSnippets[k];
+                                }
+                            }
+                        }
+                    }
+                }
+
+        } else {
+
+            return ko.toolbox2.getAbbreviationSnippet(abbreviation, viewLanguages);
+        }
 
 		return null;
 	};
